f19e94bd8e4fcbe14741feea65e21bec73f6c47a
GustaaSouzaa <140969874+GustaaSouzaa@users.noreply.github.com>
2023-11-08 13:40:08 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,34 +1,41 @@
 function calcular() {
-  const tipo = document.querySelector("#tipo").value;
-  const numero = document.querySelector("#numero").value;
+  const tipo = document.querySelector('#tipo').value;
+  const numero = document.querySelector('#numero').value;
 
   if (numero < 1 || numero > 300) {
-    document.querySelector("#resultado").innerHTML = "O número deve estar entre 1 e 300.";
+    document.querySelector('#resultado').innerHTML = "O número deve estar entre 1 e 300.";
     return;
   }
 
   if (isNaN(numero)) {
-    document.querySelector("#resultado").innerHTML = "O número deve ser um número.";
+    document.querySelector('#resultado').innerHTML = "O número deve ser um número.";
     return;
   }
 
   const resultado = 300 / numero;
 
-  // Alterado para GET
-  fetch(`calculadora.php?tipo=${tipo}&numero=${numero}`)
+  // Alterado para POST
+  fetch('calculadora.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `tipo=${tipo}&numero=${numero}`
+  })
     .then((response) => {
       if (response.status === 200) {
-        document.querySelector("#resultado").innerHTML = `${tipo}: ${resultado}%`;
+        const data = JSON.parse(response.text());
+        document.querySelector('#resultado').innerHTML = data.resultado;
       } else {
-        document.querySelector("#resultado").innerHTML = "Erro: " + response.status;
+        document.querySelector('#resultado').innerHTML = "Erro: " + response.status;
       }
     })
     .catch((error) => {
-      document.querySelector("#resultado").innerHTML = "Erro: " + error;
+      document.querySelector('#resultado').innerHTML = "Erro: " + error;
     });
 }
 
-document.querySelector("#numero").addEventListener("input", calcular);
-document.querySelector("#tipo").addEventListener("change", calcular);
+document.querySelector('#numero').addEventListener('input', calcular);
+document.querySelector('#tipo').addEventListener('change', calcular);
 
-document.querySelector("#submit").addEventListener("click", calcular);
+document.querySelector('#submit').addEventListener('click', calcular);
